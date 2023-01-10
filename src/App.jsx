337e1760bfbd741cc2e06b1b595262d3fc126f8f
Christopher Alphonse <christopheralphonse96@gmail.com>
2023-01-09 22:30:31 -0500

    adding lint and read me

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import React from "react";
+import React from 'react';
 
 function App() {
   return (
-    <div>
-      <div>
-        <h1 className='text-9xl font-bold underline'>dfdsfsfsdfsdfsdf</h1>
-      </div>
-    </div>
+    <section>
+      <article className="text-9xl"> Hello World </article>
+    </section>
   );
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/**{js,jsx}"],
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx}", "./node_modules/flowbite/**/*.js"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: "Lexend",
+      body: "Rubik",
+    },
+    container: {
+      padding: {
+        DEFAULT: ".9rem",
+        lg: "2.5rem",
+      },
+    },
+    screens: {
+      xs: "356",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    extend: {
+      colors: {
+        primary: "#050402",
+        secondary: "#1C1D24",
+        tertiary: "#1C1D24",
+        accent: {
+          DEFAULT: "rgb(28 100 242 )",
+          hover: "rgb(28 100 242 )",
+        },
+        paragraph: "#D1D1D1",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin"), require("prettier-plugin-tailwindcss")],
 };
